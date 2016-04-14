3f1a2ed2e8c559765aee7d97c68c9d574cde097b
carlosleclop <carlosleclop@gmail.com>
2016-04-14 17:12:40 +0200

    Ya crea soles que son soles pero no alumbran

--- sosy/src/Astros/Estrella.java
+++ sosy/src/Astros/Estrella.java
@@ -8,9 +8,14 @@ package Astros;
 import Model.Appearances;
 import com.sun.j3d.utils.geometry.Primitive;
 import com.sun.j3d.utils.geometry.Sphere;
+import com.sun.j3d.utils.image.TextureLoader;
 import java.util.ArrayList;
+import javax.media.j3d.Appearance;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Material;
+import javax.media.j3d.Texture;
+import javax.media.j3d.TextureAttributes;
+import javax.vecmath.Color3f;
 
 /**
  *
@@ -30,8 +35,23 @@ public class Estrella extends Astro {
         bg.addChild(
                 new Sphere (radio, 
                     Primitive.GENERATE_NORMALS | Primitive.GENERATE_TEXTURE_COORDS | Primitive.ENABLE_APPEARANCE_MODIFY, 
-                    64, Appearances.Texture.getAppearance() ));
+                    64, getAppearance() ));
         this.addChild(bg);
+    }
+    public Appearance getAppearance(){
+        Appearance ap = new Appearance();
+        Texture aTexture = new TextureLoader ("imgs/sol.jpg", null).getTexture();
+        ap.setTexture (aTexture);
+        ap.setMaterial (new Material (
+            new Color3f (0.20f, 0.20f, 0.20f),   // Color ambiental
+            new Color3f (0.00f, 0.00f, 0.00f),   // Color emisivo
+            new Color3f (0.50f, 0.50f, 0.50f),   // Color difuso
+            new Color3f (0.70f, 0.70f, 0.70f),   // Color especular
+            17.0f ));                            // Brillo
+        TextureAttributes ta = new TextureAttributes();
+        ta.setTextureMode(TextureAttributes.MODULATE);
+        ap.setTextureAttributes(ta);
+        return ap;
     }
     public void addPlaneta(Planeta p){
         planetas.add(p);
