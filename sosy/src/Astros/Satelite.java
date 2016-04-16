1f459288d27e60c86b3f468fd1fabeb1c4f6c3a3
rafaeling <rafaelingugr@gmail.com>
2016-04-16 11:45:08 +0200

    satelites 2

--- sosy/src/Astros/Satelite.java
+++ sosy/src/Astros/Satelite.java
@@ -5,17 +5,73 @@
  */
 package Astros;
 
+import com.sun.j3d.utils.geometry.Primitive;
+import com.sun.j3d.utils.geometry.Sphere;
+import com.sun.j3d.utils.image.TextureLoader;
+import java.util.ArrayList;
+import javax.media.j3d.Appearance;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Material;
+import javax.media.j3d.Texture;
+import javax.media.j3d.TextureAttributes;
+import javax.vecmath.Color3f;
+
 /**
  *
  * @author CARLOS
  */
 public class Satelite extends AstroOpaco {
     
-    Satelite () {
-        
+    ArrayList <Satelite> satelites;
+    ArrayList <Anillo> anillos;
+    private BranchGroup bg;
+    
+    Satelite(){
         
     }
+    Satelite(float radio){
+        this.radio = radio;
+        bg = new BranchGroup();
+        bg.addChild(
+                new Sphere (radio, 
+                    Primitive.GENERATE_NORMALS | Primitive.GENERATE_TEXTURE_COORDS | Primitive.ENABLE_APPEARANCE_MODIFY, 
+                    64, getAppearance("imgs/moon.jpg") ));
+        this.addChild(bg);
+    }
+    Satelite(float radio, String textureString){
+        this.radio = radio;
+        bg = new BranchGroup();
+        bg.addChild(
+                new Sphere (radio, 
+                    Primitive.GENERATE_NORMALS | Primitive.GENERATE_TEXTURE_COORDS | Primitive.ENABLE_APPEARANCE_MODIFY, 
+                    64, getAppearance(textureString) ));
+        this.addChild(bg);
+    }
+
     
-    
+    private Appearance getAppearance(String textureString){
+        Appearance ap = new Appearance();
+        Texture aTexture = new TextureLoader (textureString, null).getTexture();
+        ap.setTexture (aTexture);
+
+/*        ap.setMaterial (new Material (
+            new Color3f (0.20f, 0.20f, 0.20f),   // Color ambiental
+            new Color3f (0.00f, 0.00f, 0.00f),   // Color emisivo
+            new Color3f (0.50f, 0.50f, 0.50f),   // Color difuso
+            new Color3f (0.70f, 0.70f, 0.70f),   // Color especular
+            5.0f ));                            // Brillo
+        */
+        ap.setMaterial (new Material (
+            new Color3f (0.20f, 0.20f, 0.20f),   // Color ambiental
+            new Color3f (0.00f, 0.00f, 0.00f),   // Color emisivo
+            new Color3f (0.50f, 0.50f, 0.50f),   // Color difuso
+            new Color3f (0.40f, 0.40f, 0.40f),   // Color especular
+            6.0f ));                            // Brillo
+        TextureAttributes ta = new TextureAttributes();
+        ta.setTextureMode(TextureAttributes.MODULATE);
+        ap.setTextureAttributes(ta);
+        return ap;
+    }
     
 }
+
